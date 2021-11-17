d3d18f4a83d6cd1bc5c41a49a0ca535bd70f0c0a
alibabashovv <90691151+alibabashovv@users.noreply.github.com>
2021-11-17 12:37:01 -0800

    Delete Reversestr directory

--- Reversestr/js/script.js
+++
@@ -1,8 +0,0 @@
-function ReverseString(str) {
-    var reversestr = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        reversestr += str[i];
-    }
-    return reversestr;
-}
-console.log(ReverseString("Hello my "));
